5fed5e023cf859836bafe18d36c7478db6301241
Mentide <68889450+Mentide@users.noreply.github.com>
2021-10-04 20:09:13 +0500

    Update user.js

--- user.js
+++ user.js
@@ -7,7 +7,7 @@ var user = new mongoose.Schema({
  muted: Boolean,
   muteinfo: String,
   time: String,
-  guild:String,
+  guildid:String,
   unmute: Date,
   text:String,
   muteinfo1: String,
@@ -16,7 +16,14 @@ var user = new mongoose.Schema({
   muteinfo2: String,
   time2: String,
   unmute2: Date,
-  msg:{type:Number,default:0}
+  msg:{type:Number,default:0},
+  muted11: Boolean,
+  muteinfo11: String,
+  time11: String,
+  unmute11: Date,
+  left11: Boolean,
+  channel11: { type: Number, default: null},
+  reason11: { type: String, default: 'не в муте'},
 });
 
 module.exports = mongoose.model("users", user);
