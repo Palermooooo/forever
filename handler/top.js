a482f3c72f95c1269bb384cf84db4fd509827612
Mentide <68889450+Mentide@users.noreply.github.com>
2021-10-06 18:13:38 +0500

    Update top.js

--- handler/top.js
+++ handler/top.js
@@ -1,7 +1,7 @@
 const User = require("../user.js");
 const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
-  let users = await User.find({guild:message.guild.id, totalxp: {$ne: null}}).sort( { totalxp: -1 } ).limit(10);
+  let users = await User.find({msg: {$ne: null}}).sort( { msg: -1 } ).limit(10);
   let text = '';
 let n = 0;
   for(const user of users){
@@ -19,5 +19,5 @@ let n = 0;
   message.channel.send(ihatekids)
   };
 module.exports.help = {
-    name: "top",
+    name: "топ",
 }
